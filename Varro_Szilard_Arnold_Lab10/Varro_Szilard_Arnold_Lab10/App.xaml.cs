19fba098ccfb7efe4dbf11cff6192d19a802de84
varroszilard <szilard_szili@yahoo.com>
2021-12-10 16:56:22 +0200

    Completat cu Lab10

--- Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/App.xaml.cs
+++ Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/App.xaml.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
+using Varro_Szilard_Arnold_Lab10.Data;
 
 namespace Varro_Szilard_Arnold_Lab10
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListEntryPage());
         }
 
         protected override void OnStart()
---
+++ Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Varro_Szilard_Arnold_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
