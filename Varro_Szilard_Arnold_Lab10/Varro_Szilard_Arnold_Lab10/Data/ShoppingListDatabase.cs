bba921b9bae861ffde0d7c6cf5fe516ab3de9bd3
varroszilard <szilard_szili@yahoo.com>
2021-12-10 17:31:50 +0200

    Completare cu Lab11

--- Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/Data/ShoppingListDatabase.cs
+++ Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/Data/ShoppingListDatabase.cs
@@ -14,17 +14,22 @@ namespace Varro_Szilard_Arnold_Lab10.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
         }
+
         public Task<ShopList> GetShopListAsync(int id)
         {
             return _database.Table<ShopList>()
                 .Where(i => i.ID == id)
                 .FirstOrDefaultAsync();
         }
+
         public Task<int> SaveShopListAsync(ShopList slist)
         {
             if (slist.ID != 0)
@@ -36,9 +41,53 @@ namespace Varro_Szilard_Arnold_Lab10.Data
                 return _database.InsertAsync(slist);
             }
         }
+
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
             return _database.DeleteAsync(slist);
+        }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+                "select P.ID, P.Description from Product P"
+                + " inner join ListProduct LP"
+                + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+                shoplistid);
         }
     }
 }
--- Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/ListPage.xaml.cs
+++ Varro_Szilard_Arnold_Lab10/Varro_Szilard_Arnold_Lab10/ListPage.xaml.cs
@@ -31,5 +31,22 @@ namespace Varro_Szilard_Arnold_Lab10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList) this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
